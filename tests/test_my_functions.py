feb283aab1a3833023679747e005429b5e93beb9
DouglasL7 <lopesdouglas92@gmail.com>
2024-03-10 16:58:46 -0300

    Tests initialized

---
+++ tests/test_my_functions.py
@@ -0,0 +1,21 @@
+import pytest
+import source.my_functions as my_functions
+
+
+def test_add():
+  result = my_functions.add(1, 4)
+  assert result == 5
+
+
+def test_add_string():
+  result = my_functions.add("I like", " to drink")
+  assert result == "I like to drink"
+  
+def test_divide():
+  result = my_functions.divide(10, 5)
+  assert result == 2
+ 
+
+def test_divide_by_zero():
+  with pytest.raises(ValueError):
+    my_functions.divide(10, 0)
